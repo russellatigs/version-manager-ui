78b3a80648bcf2d3bb5af936c223d4e047253c2a
Jair Moreno <jairmorenogomez@gmail.com>
2016-06-06 14:36:22 -0400

    added basic ajax structure, having some problems creating cors request to populatetables

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,11 +2,86 @@ $(document).ready(function () {
        $('#PersonTableContainer').jtable({
            title: 'Table of people',
            actions: {
-               listAction: '/GettingStarted/PersonList',
-               createAction: '/GettingStarted/CreatePerson',
-               updateAction: '/GettingStarted/UpdatePerson',
-               deleteAction: '/GettingStarted/DeletePerson'
-           },
+              listAction: function (postData, jtParams) {
+                    console.log("Loading from custom function...");
+                    return $.Deferred(function ($dfd) {
+                       $.ajax({
+                            type: 'POST',
+                            url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs',
+                            crossDomain: true,
+                            dataType: 'json',
+                            data: JSON.stringify(),
+                            headers: {
+                              'Content-Type': 'application/json'
+
+                            },
+                            success: function (data) {
+                               $dfd.resolve(data);
+                               console.log('worked!');
+                            },
+                            error: function () {
+                               $dfd.reject();
+                               console.log('did not work!');
+                          }
+                        });
+                    });
+                },
+                createAction: function (postData) {
+                    console.log("creating from custom function...");
+                    return $.Deferred(function ($dfd) {
+                        $.ajax({
+                            type: 'POST',
+                            url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs',
+                            crossDomain: true,
+                            dataType: 'json',
+                            data: postData,
+                            success: function (data) {
+                                $dfd.resolve(data);
+                            },
+                            error: function () {
+                                $dfd.reject();
+                            }
+                        });
+                    });
+                },
+                deleteAction: function (postData) {
+                    console.log("deleting from custom function...");
+                    return $.Deferred(function ($dfd) {
+                        $.ajax({
+                            type: 'DELETE',
+                            url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs/{jobid}',
+                            crossDomain: true,
+                            dataType: 'json',
+                            data: postData,
+                            success: function (data) {
+                                $dfd.resolve(data);
+                            },
+                            error: function () {
+                                $dfd.reject();
+                            }
+                        });
+                    });
+                },
+
+                updateAction: function(postData) {
+                    console.log("exporting...");
+                    return $.Deferred(function ($dfd) {
+                        $.ajax({
+                            type: 'GET',
+                            url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs/{jobid}/file',
+                            crossDomain: true,
+                            dataType: 'json',
+                            data: postData,
+                            success: function (data) {
+                                $dfd.resolve(data);
+                            },
+                            error: function () {
+                                $dfd.reject();
+                            }
+                        });
+                    });
+                }
+            },
            fields: {
                PersonId: {
                    key: true,
@@ -39,4 +114,5 @@ $(document).ready(function () {
 
            }
        });
+       $('#PersonTableContainer').jtable('load');
    });
