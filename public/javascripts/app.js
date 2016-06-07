fccdb3b364644aff8380c3e3ae2ea9d99e547473
Jair Moreno <jairmorenogomez@gmail.com>
2016-06-07 12:37:45 -0400

    added client side requests, next step is to populate data and delete in UI

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,23 +1,100 @@
 $(document).ready(function(){
 
-
-                       $.ajax({
-
-                            url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs',
+                  //  Gets jobs and populates data
+                        $.ajax({
+                            type: 'GET',
+                            url: "http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs",
                             timeout: 3000,
                             crossDomain: true,
                             dataType: 'json',
                             headers: {
-                              'VMUser': 'hmoreno'
-                                      },
-                            success: function (data) {
-                               $dfd.resolve(data);
-                               console.log('worked!');
-                            },
-                            error: function () {
-                               $dfd.reject();
-                               console.log('did not work!');
-                          }
+                                  "VMUser": "hmoreno"
+                                   }
+                               }).done(function (data) {
+                                   console.log(data);
+                                   console.log("result recieved");
+                               }).fail(function (err) {
+                                   console.log(err);
+                                   console.log("error")
+                               })
+                                    .always(function () {
+                                      console.log("complete");
+                              });
+
+
+                      //Creates a job
+                      $( "#add" ).click(function() {
+                        $.ajax({
+                        type: 'POST',
+                        url: "http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs",
+                        timeout: 3000,
+                        rossDomain: true,
+                        dataType: 'json',
+                        headers: {
+                              "VMUser": "hmoreno"
+                               }
+                           }).done(function (data) {
+                               console.log(data);
+                               console.log("result recieved");
+                           }).fail(function (err) {
+                               console.log(err);
+                               console.log("error")
+                           })
+                                .always(function () {
+                                  console.log("complete");
+                          });
+
                         });
-                    });
+
+                      //Deletes a Job
+                      $( "#delete" ).click(function() {
+                        console.log('Im working');
+                      $.ajax({
+                        type: 'POST',
+                        url: "http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs/{jobid}",
+                        timeout: 3000,
+                        crossDomain: true,
+                        dataType: 'json',
+                        headers: {
+                              "VMUser": "hmoreno"
+                               }
+                           }).done(function (data) {
+                               console.log(data);
+                               console.log("result recieved");
+                           }).fail(function (err) {
+                               console.log(err);
+                               console.log("error")
+                           })
+                                .always(function () {
+                                  console.log("complete");
+                          });
+
+                   });
+
+                    //  Export a Job
+                      $( "#export" ).submit(function() {
+                      $.ajax({
+                        type: 'GET',
+                        url: "http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs/{jobid}/file",
+                        timeout: 3000,
+                        crossDomain: true,
+                        dataType: 'json',
+                        headers: {
+                              "VMUser": "hmoreno"
+                               }
+                           }).done(function (data) {
+                               console.log(data);
+                               console.log("result recieved");
+                           }).fail(function (err) {
+                               console.log(err);
+                               console.log("error")
+                           })
+                                .always(function () {
+                                  console.log("complete");
+                          });
+
+                  });
+
+
+
 });
