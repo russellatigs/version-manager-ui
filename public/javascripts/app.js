c8da81425c241eb8a713cf9905443f78e1c82df0
Jair Moreno <jairmorenogomez@gmail.com>
2016-06-07 10:49:26 -0400

    removed angular and other views files

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,15 @@
-$(document).ready(function () {
-       $('#PersonTableContainer').jtable({
-           title: 'Table of people',
-           actions: {
-              listAction: function (postData, jtParams) {
-                    console.log("Loading from custom function...");
-                    return $.Deferred(function ($dfd) {
+$(document).ready(function(){
+
+
                        $.ajax({
-                            type: 'POST',
+
                             url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs',
+                            timeout: 3000,
                             crossDomain: true,
                             dataType: 'json',
-                            data: JSON.stringify(),
                             headers: {
-                              'Content-Type': 'application/json'
-
-                            },
+                              'VMUser': 'hmoreno'
+                                      },
                             success: function (data) {
                                $dfd.resolve(data);
                                console.log('worked!');
@@ -25,94 +20,4 @@ $(document).ready(function () {
                           }
                         });
                     });
-                },
-                createAction: function (postData) {
-                    console.log("creating from custom function...");
-                    return $.Deferred(function ($dfd) {
-                        $.ajax({
-                            type: 'POST',
-                            url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs',
-                            crossDomain: true,
-                            dataType: 'json',
-                            data: postData,
-                            success: function (data) {
-                                $dfd.resolve(data);
-                            },
-                            error: function () {
-                                $dfd.reject();
-                            }
-                        });
-                    });
-                },
-                deleteAction: function (postData) {
-                    console.log("deleting from custom function...");
-                    return $.Deferred(function ($dfd) {
-                        $.ajax({
-                            type: 'DELETE',
-                            url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs/{jobid}',
-                            crossDomain: true,
-                            dataType: 'json',
-                            data: postData,
-                            success: function (data) {
-                                $dfd.resolve(data);
-                            },
-                            error: function () {
-                                $dfd.reject();
-                            }
-                        });
-                    });
-                },
-
-                updateAction: function(postData) {
-                    console.log("exporting...");
-                    return $.Deferred(function ($dfd) {
-                        $.ajax({
-                            type: 'GET',
-                            url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs/{jobid}/file',
-                            crossDomain: true,
-                            dataType: 'json',
-                            data: postData,
-                            success: function (data) {
-                                $dfd.resolve(data);
-                            },
-                            error: function () {
-                                $dfd.reject();
-                            }
-                        });
-                    });
-                }
-            },
-           fields: {
-               PersonId: {
-                   key: true,
-                   list: false
-               },
-               JobName: {
-                   title: 'Job Name',
-                   width: '40%'
-               },
-               CreatedBy: {
-                   title: 'Created By',
-                   width: '20%'
-               },
-               Long: {
-                   title: 'Longitude',
-                   width: '20%'
-               },
-               Lat: {
-                   title: 'Latitude',
-                   width: '20%'
-               },
-
-               RecordDate: {
-                   title: 'Record date',
-                   width: '30%',
-                   type: 'date',
-                   create: false,
-                   edit: false
-               }
-
-           }
-       });
-       $('#PersonTableContainer').jtable('load');
-   });
+});
