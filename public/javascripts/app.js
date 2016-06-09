647a69838ea0e2896ab5d9778d3540c2dcef6e03
Jair Moreno <jairmorenogomez@gmail.com>
2016-06-09 11:16:57 -0400

    Added UI buttons for check in, post to gold, and fixed the modals

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
                 var id = value.jobid;
                 var status = value.status;
 
-                $("#jobs tbody").append("<tr><td>"+ status +"</td><td>" + jobName + "</td><td>" + createdBy + "</td><td>" + latitude + "</td><td>" + longitude + "</td><td> <button class='btn btn-warning btn-small' type='submit' id='edit'> <span class='glyphicon glyphicon-edit' aria-hidden='true'></span> Edit </button> <button class='btn btn-primary btn-small' type='submit' id='export' method='GET'> <span class='glyphicon glyphicon-send' aria-hidden='true'></span> Export </button> <button class='btn btn-danger btn-small' type='submit' id='delete' method='DELETE'> <span class='glyphicon glyphicon-remove' aria-hidden='true'></span> Delete </button> </td></tr> ");
+                $("#jobs tbody").append("<tr><td>"+ status +"</td><td>" + jobName + "</td><td>" + createdBy + "</td><td>" + latitude + "</td><td>" + longitude + "</td><td>  <button class='btn btn-primary btn-small' id='export' data-toggle='modal' data-target='#exportModal'> <span class='glyphicon glyphicon-send' aria-hidden='true'></span> Export </button> <button class='btn btn-info btn-small' type='submit' id='edit' data-toggle='modal' data-target='#checkModal'> <span class='glyphicon glyphicon-ok' aria-hidden='true'></span> Check In </button> <button class='btn btn-warning btn-small' type='submit' id='edit' data-toggle='modal' data-target='#postModal'> <span class='glyphicon glyphicon-edit' aria-hidden='true'></span> Post to Gold </button> <button class='btn btn-danger btn-small' id='delete' data-toggle='modal' data-target='#deleteModal'> <span class='glyphicon glyphicon-remove' aria-hidden='true'></span> Delete </button> </td></tr> ");
             });
 
         },
@@ -39,30 +39,7 @@ $(document).ready(function () {
             console.log("complete");
         });
 
-    //Creates a job
-    // $("#add").click(function () {
-    //     $.ajax({
-    //         type: 'POST',
-    //         url: "http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs",
-    //         timeout: 3000,
-    //         rossDomain: true,
-    //         dataType: 'json',
-    //         headers: {
-    //             "VMUser": "hmoreno"
-    //         },
-    //     }).done(function (data) {
-    //         console.log(data);
-    //         console.log("result recieved");
-    //     }).fail(function (err) {
-    //             console.log(err);
-    //             console.log("error")
-    //         })
-    //         .always(function () {
-    //             console.log("complete");
-    //         });
-    //
-    // });
-
+   //Creates a Job
     $('#job-form').submit(function(e) {
        e.preventDefault();
         $.ajax({
@@ -70,16 +47,14 @@ $(document).ready(function () {
             url: 'http://ec2-54-152-233-204.compute-1.amazonaws.com:8888/jobs',
             headers:{"VMUser":"hmoreno",
             'Accept': 'application/json',
-          'Content-Type': 'application/json' },
-          dataType: 'json',
+            'Content-Type': 'application/json' },
+            dataType: 'json',
             timeout:3000,
             crossDomain: true,
             success: function(data){
               console.log(data);
             },
-            data: JSON.stringify({ name: $("#job-name").val(),
-                    longitude: $("#job-longitude").val(),
-                    latitude:  $("#job-latitude").val()})
+            data: JSON.stringify
         });
         return false;
     });
@@ -132,4 +107,5 @@ $(document).ready(function () {
             });
 
     });
+
 });
